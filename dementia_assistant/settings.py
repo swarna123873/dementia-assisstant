9a0f5a28ec8b528313bb92980dfad17375578f1e
Dementia Assistant Developer <developer@dementia-assistant.local>
2026-02-02 19:54:26 +0530

    Configure PostgreSQL support

--- dementia_assistant/settings.py
+++ dementia_assistant/settings.py
@@ -77,12 +77,19 @@ WSGI_APPLICATION = 'dementia_assistant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+database_url = os.environ.get("DATABASE_URL")
+if database_url:
+    DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 # Password validation
